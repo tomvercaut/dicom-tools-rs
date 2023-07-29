e099f97b9a342c336dee1d5870e3f791c0a4c046
Tom Vercauteren <tomvercaut@gmail.com>
2023-07-29 23:47:33 +0200

    Refactor field names in GeneralStudy struct.
    Redundant prefixes in the field names in GeneralStudy structure are removed to improve readability and conciseness.

--- core/src/modules/general_study.rs
+++ core/src/modules/general_study.rs
@@ -15,17 +15,17 @@ use std::convert::TryFrom;
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct GeneralStudy {
     /// Date study was performed
-    pub study_date: String,
+    pub date: String,
     /// Time study was performed
-    pub study_time: String,
+    pub time: String,
     ///A RIS generated number that identifies the order for the Study.
     pub accession_number: String,
     /// Name of the Patient's referring physician
     pub referring_physicians_name: String,
     /// Unique identifier for the Study.
-    pub study_instance_uid: String,
+    pub instance_uid: String,
     /// User or equipment generated Study identifier.
-    pub study_id: String,
+    pub id: String,
 }
 
 impl<D> TryFrom<&InMemDicomObject<D>> for GeneralStudy
@@ -36,13 +36,13 @@ where
 
     fn try_from(obj: &InMemDicomObject<D>) -> Result<Self, Self::Error> {
         let mut study = GeneralStudy::default();
-        study.study_date = element_opt_to_str(obj, STUDY_DATE, "")?.to_string();
-        study.study_time = element_opt_to_str(obj, STUDY_TIME, "")?.to_string();
+        study.date = element_opt_to_str(obj, STUDY_DATE, "")?.to_string();
+        study.time = element_opt_to_str(obj, STUDY_TIME, "")?.to_string();
         study.accession_number = element_opt_to_str(obj, ACCESSION_NUMBER, "")?.to_string();
         study.referring_physicians_name =
             element_opt_to_str(obj, REFERRING_PHYSICIAN_NAME, "")?.to_string();
-        study.study_instance_uid = obj.element(STUDY_INSTANCE_UID)?.string()?.to_string();
-        study.study_id = element_opt_to_str(obj, STUDY_ID, "")?.to_string();
+        study.instance_uid = obj.element(STUDY_INSTANCE_UID)?.string()?.to_string();
+        study.id = element_opt_to_str(obj, STUDY_ID, "")?.to_string();
         Ok(study)
     }
 }
@@ -69,12 +69,12 @@ mod tests {
         let res = GeneralStudy::try_from(&obj);
         assert!(res.is_ok());
         let expected = GeneralStudy {
-            study_date: "20230729".to_string(),
-            study_time: "21:59:59".to_string(),
+            date: "20230729".to_string(),
+            time: "21:59:59".to_string(),
             accession_number: "12".to_string(),
             referring_physicians_name: "Doe^John".to_string(),
-            study_instance_uid: "1.2.3.4".to_string(),
-            study_id: "13".to_string(),
+            instance_uid: "1.2.3.4".to_string(),
+            id: "13".to_string(),
         };
         let sop = res.unwrap();
         assert_eq!(expected, sop);
@@ -87,12 +87,12 @@ mod tests {
         let res = GeneralStudy::try_from(&obj);
         assert!(res.is_ok());
         let expected = GeneralStudy {
-            study_date: "".to_string(),
-            study_time: "".to_string(),
+            date: "".to_string(),
+            time: "".to_string(),
             accession_number: "".to_string(),
             referring_physicians_name: "".to_string(),
-            study_instance_uid: "1.2.3.4".to_string(),
-            study_id: "".to_string(),
+            instance_uid: "1.2.3.4".to_string(),
+            id: "".to_string(),
         };
         let sop = res.unwrap();
         assert_eq!(expected, sop);
